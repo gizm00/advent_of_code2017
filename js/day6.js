3affe9f44b7561b49f570c4605c103acd0fa16ff
Sev Leonard <jennifer.m.leonard@gmail.com>
2017-12-06 20:14:12 -0800

    got part1 working

--- js/day6.js
+++ js/day6.js
@@ -4,23 +4,27 @@ const day61= function(matrix) {
   let dupeSequence = false
   let sequences = []
   let cycles = 0
-  let sequence = matrix.split('\t')
+  let sequence = matrix.split(/\s+/)
+  sequence = sequence.map(Number)
   let sequenceLen = sequence.length
 
   console.log('starting sequence: ' + sequence)
-  while (!sequences.includes(sequence.join(' '))) {
-    sequences.push(sequence.join(' '))
+  let sequenceRep = sequence.join(',')
+  while (!sequences.includes(sequenceRep)) {
+      sequences.push(sequenceRep)
       // reallocate
       let max = _.max(sequence)
       let currentIndex = _.indexOf(sequence, max)
       sequence[currentIndex] = 0
-
+      //console.log('max: ' + max + 'at currentIndex ' + currentIndex)
       while (max > 0) {
         currentIndex = (currentIndex + 1) % sequenceLen
+        //console.log('updated current index ' + currentIndex)
         sequence[currentIndex]++
         max--
       }
-      console.log('sequence for next cycle: ' + sequence)
+      sequenceRep = sequence.join(',')
+      //console.log('sequence for next cycle: ' + sequenceRep)
       cycles++
 
   }
