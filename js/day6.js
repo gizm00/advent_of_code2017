8dd19d74c0d540048df127662eae54a85de88435
Sev Leonard <jennifer.m.leonard@gmail.com>
2017-12-07 20:39:03 -0800

    finished day6

--- js/day6.js
+++ js/day6.js
@@ -1,16 +1,18 @@
 // given a tab delimited list of blocks redistribute
 // blocks evenly across list until a sequence is seen again
-const day61= function(matrix) {
+const day61= function(matrix, occurences=1) {
   let dupeSequence = false
   let sequences = []
   let cycles = 0
+  let countOccurences = 0
+  let sequenceToFind = undefined
   let sequence = matrix.split(/\s+/)
   sequence = sequence.map(Number)
   let sequenceLen = sequence.length
 
   console.log('starting sequence: ' + sequence)
   let sequenceRep = sequence.join(',')
-  while (!sequences.includes(sequenceRep)) {
+  while (!dupeSequence && countOccurences < occurences) {
       sequences.push(sequenceRep)
       // reallocate
       let max = _.max(sequence)
@@ -24,8 +26,24 @@ const day61= function(matrix) {
         max--
       }
       sequenceRep = sequence.join(',')
+
       //console.log('sequence for next cycle: ' + sequenceRep)
       cycles++
+
+      // if (sequences.includes(sequenceToFind)) {
+      //     console.log('found sequence again after ' + cycles + ' cycles')
+      //     dupeSequence = true
+      // }
+
+      if(sequences.includes(sequenceRep)) {
+        console.log('sequence found after ' + cycles + ' cycles')
+        console.log(sequence)
+        sequenceToFind = sequence.join(',')
+        sequences = []
+        countOccurences++
+        cycles = 0
+      }
+
 
   }
   console.log('redistribution in ' + cycles + ' cycles')
